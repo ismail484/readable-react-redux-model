8f5ada1c61644045db29a1b702d320ec3cf73c86
Ismail <Mohamed.Ahmed.Ismail@dnvgl.com>
2017-10-14 08:21:24 +0200

    fetch data from server

---
+++ src/api/CommentsApi.js
@@ -0,0 +1,64 @@
+
+const api = "http://localhost:5001"
+
+let token = localStorage.token
+if (!token)
+   token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token,
+  'Content-Type': 'application/json'
+}
+
+// GET /posts/:id/comments
+export const getAllComments = (id) =>
+   fetch(`${api}/posts/${id}/comments`, { headers })
+     .then(res => res.json())
+
+
+
+
+
+// POST /comments
+export const addComment = (comment) =>
+  fetch(`${api}/comments`, {
+   method: 'POST',
+   headers: headers,
+   body: JSON.stringify(comment)
+  }).then(res => res.json())
+
+
+  // DELETE /comments/:id
+export const deleteComment = (id) => {
+  return fetch(`${api}/comments/${id}`, {
+    method: 'DELETE',
+    headers
+  }).then(data => data.json())
+}
+
+
+
+// PUT  /comments/:id
+export const editComment = (id,comment) => {
+  return fetch(`${api}/comments/${id}`, { 
+    method: 'PUT',
+    headers ,
+     body: JSON.stringify(comment)
+  }) .then(response => response.json())
+
+}
+
+export const voteComment = (id, option) => {
+  return fetch(`${api}/comments/${id}`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      option: option
+    })
+  })
+  .then(data => data.json())
+}
---
+++ src/api/PostsApi.js
@@ -0,0 +1,67 @@
+
+const api = "http://localhost:5001"
+
+let token = localStorage.token
+if (!token)
+   token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token,
+  'Content-Type': 'application/json'
+}
+
+
+// GET /posts
+export const getAllPosts = ()=> 
+   fetch(`${api}/posts`, { headers })
+    .then(response => response.json())
+
+
+// DELETE /posts/:id
+export const deletePost = (id) => {
+  return fetch(`${api}/posts/${id}`, { 
+    method: 'DELETE',
+    headers 
+  })
+}
+
+// Edit /posts/:id
+export const editPost = (id,post) => {
+  return fetch(`${api}/posts/${id}`, { 
+    method: 'PUT',
+    headers ,
+     body: JSON.stringify(post)
+  }) .then(response => response.json())
+
+}
+
+
+
+export const votePost = (id, option) => {
+  return fetch(`${api}/posts/${id}`, {
+    method: 'POST',
+    headers:{
+    ...headers,
+       'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      option: option
+    })
+  })
+}
+
+
+// Post /posts/:id
+export const getPost = (id) => {
+  return fetch(`${api}/posts/${id}`, { headers })
+    .then(response => response.json())
+}
+
+export const addPost = (post) =>{
+ return fetch(`${api}/posts`, {
+   method: 'POST',
+   headers: headers,
+   body: JSON.stringify(post)
+  }).then(res => res.json())
+
+}
